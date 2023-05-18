ca82f5b57f50d80738384318212c842e0fae2e33
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-05-18 18:29:32 +0700

    edit profile screen

--- capstone_flutter/lib/view/screen/profile_screen/profile_screen.dart
+++ capstone_flutter/lib/view/screen/profile_screen/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:capstone_flutter/utils/const/theme.dart';
 import 'package:capstone_flutter/view/screen/profile_screen/change_profile_screen.dart';
 import 'package:capstone_flutter/view/screen/profile_screen/security_setting_screen.dart';
+import 'package:capstone_flutter/view/screen/profile_screen/terms_condition_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -126,7 +127,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       ),
                     ),
                     InkWell(
-                      onTap: () {},
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const TermsCondition()));
+                      },
                       child: Padding(
                         padding: const EdgeInsets.only(
                             left: 10, top: 15, bottom: 15, right: 15),
