f926fda333067e36393473e0605e8f9d576ef517
Megy Permana <smamegy@gmail.com>
2023-05-18 12:49:35 +0700

    Add Riwayat Tagihan

---
+++ capstone_flutter/lib/view/screen/riwayat_tagihan/riwayat_tagihan_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RiwayatTagihanScreen extends StatefulWidget {
+  const RiwayatTagihanScreen({super.key});
+
+  @override
+  State<RiwayatTagihanScreen> createState() => _RiwayatTagihanScreenState();
+}
+
+class _RiwayatTagihanScreenState extends State<RiwayatTagihanScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Riwayat Tagihan"),
+      ),
+    );
+  }
+}
