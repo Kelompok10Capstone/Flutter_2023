2e86a9c4c76de6d5f3d9848a27e8b44d21e0ae86
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-05-22 23:31:52 +0700

    edit theme

--- capstone_flutter/lib/utils/const/theme.dart
+++ capstone_flutter/lib/utils/const/theme.dart
@@ -8,7 +8,37 @@ TextStyle blackFont18 =
     GoogleFonts.roboto().copyWith(fontSize: 18, fontWeight: FontWeight.w700);
 TextStyle whiteFont12 =
     GoogleFonts.roboto().copyWith(fontSize: 14, fontWeight: FontWeight.w400);
+
 TextStyle whiteFont14 =
     GoogleFonts.roboto().copyWith(fontSize: 14, fontWeight: FontWeight.w400);
+
 TextStyle blackFont16 =
     GoogleFonts.roboto().copyWith(fontSize: 16, fontWeight: FontWeight.w700);
+
+/// Tambahan text dari iyan
+// color white
+TextStyle whiteFont15 = GoogleFonts.roboto()
+    .copyWith(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.white);
+// color blue
+TextStyle blueFont12 = GoogleFonts.roboto().copyWith(
+    fontSize: 12, fontWeight: FontWeight.w700, color: const Color(0xff2B3990));
+TextStyle blueFont16 = GoogleFonts.roboto().copyWith(
+    fontSize: 16, fontWeight: FontWeight.w700, color: const Color(0xff2B3990));
+
+// color black
+TextStyle blackFont12 = GoogleFonts.roboto().copyWith(
+    fontSize: 12, fontWeight: FontWeight.w400, color: const Color(0xff161616));
+TextStyle blackFormFont12 = GoogleFonts.roboto().copyWith(
+    fontSize: 12, fontWeight: FontWeight.w700, color: const Color(0xff161616));
+TextStyle blackText12 = GoogleFonts.roboto().copyWith(
+    fontSize: 12, fontWeight: FontWeight.w400, color: const Color(0xff111111));
+TextStyle blackFont14 = GoogleFonts.roboto()
+    .copyWith(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black);
+TextStyle blackText16 = GoogleFonts.roboto().copyWith(
+    fontSize: 16, fontWeight: FontWeight.w700, color: const Color(0xff111111));
+TextStyle blackFontt16 = GoogleFonts.roboto().copyWith(
+    fontSize: 16, fontWeight: FontWeight.w400, color: const Color(0xff111111));
+TextStyle blackFontAppbar18 = GoogleFonts.roboto()
+    .copyWith(fontSize: 18, fontWeight: FontWeight.w700, color: Colors.black);
+TextStyle blackText24 = GoogleFonts.roboto().copyWith(
+    fontSize: 24, fontWeight: FontWeight.w700, color: const Color(0xff111111));
