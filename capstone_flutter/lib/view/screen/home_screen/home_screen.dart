a1e3afb363fee657658187627f29279f523b7fb1
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-05-24 19:05:04 +0700

    add dispose

--- capstone_flutter/lib/view/screen/home_screen/home_screen.dart
+++ capstone_flutter/lib/view/screen/home_screen/home_screen.dart
@@ -36,6 +36,7 @@ class _HomeScreenState extends State<HomeScreen>
   TextEditingController pelangganControllerWifi = TextEditingController();
   @override
   void dispose() {
+    super.dispose();
     pelangganControllerBpjs.dispose();
     _tabController?.dispose();
     pelangganControllerToken.dispose();
@@ -44,6 +45,7 @@ class _HomeScreenState extends State<HomeScreen>
 
   @override
   void initState() {
+    super.initState();
     _tabController = TabController(length: 2, vsync: this);
   }
 
