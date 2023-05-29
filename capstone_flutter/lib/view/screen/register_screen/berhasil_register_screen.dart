5c2e4c1a677a0386b448ec4e465210c08fca7653
Iyann986 <hisdiantonoktavian@gmail.com>
2023-05-30 02:41:13 +0700

    Memperbaiki tampilan berhasil register

--- capstone_flutter/lib/view/screen/register_screen/berhasil_register_screen.dart
+++ capstone_flutter/lib/view/screen/register_screen/berhasil_register_screen.dart
@@ -5,7 +5,6 @@ import '../../../utils/const/theme.dart';
 
 class BerhasilRegisterScreen extends StatefulWidget {
   const BerhasilRegisterScreen({super.key});
-  static const String route = "/berhasil_register";
 
   @override
   State<BerhasilRegisterScreen> createState() => _BerhasilRegisterScreenState();
@@ -16,48 +15,51 @@ class _BerhasilRegisterScreenState extends State<BerhasilRegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 200, bottom: 0, left: 50, right: 50),
-              child: Image.asset(
-                'assets/images/ic_berhasil_register.png',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 200, bottom: 10, left: 66, right: 66),
+            child: Image.asset(
+              'assets/cihuy_selamat.png',
+            ),
+          ),
+          Text(
+            'Cihuyy ! Selamat',
+            style: blackText24,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Kamu telah berhasil mendaftar',
+            style: blackFontt16,
+          ),
+        ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 30, left: 24, right: 24),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: 52,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: blueColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
             ),
-            Text(
-              'Cihuyy ! Selamat',
-              style: blackText24,
+            onPressed: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(
+              //     builder: (context) => const BerhasilTransaksiPulsa(),
+              //   ),
+              // );
+            },
+            child: Text(
+              'Lanjutkan',
+              style: whiteFont14,
             ),
-            const SizedBox(height: 15),
-            Text(
-              'Kamu telah berhasil mendaftar',
-              style: blackFontt16,
-            ),
-            const SizedBox(
-              height: 200,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Navigasi ke home menggunakan route
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xff2B3990),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 125,
-                  vertical: 18,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: Text(
-                'Lanjutkan',
-                style: whiteFont15,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
