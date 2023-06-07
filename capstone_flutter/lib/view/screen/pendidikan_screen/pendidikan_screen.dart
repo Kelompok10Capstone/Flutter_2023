58905e008bebb6872caf790bcc0de3f35459fc1d
Iyann986 <hisdiantonoktavian@gmail.com>
2023-06-07 23:45:57 +0700

    Menambahkan pendidikan screen

--- capstone_flutter/lib/view/screen/pendidikan_screen/pendidikan_screen.dart
+++ capstone_flutter/lib/view/screen/pendidikan_screen/pendidikan_screen.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../utils/const/theme.dart';
 
-class PendidikanScreen extends StatelessWidget {
+class PendidikanScreen extends StatefulWidget {
   const PendidikanScreen({super.key});
+
+  @override
+  State<PendidikanScreen> createState() => _PendidikanScreenState();
+}
+
+class _PendidikanScreenState extends State<PendidikanScreen> {
+  // Text Editing Controller
+  final TextEditingController institusiController = TextEditingController();
+  final TextEditingController pembayaranController = TextEditingController();
+
+  @override
+  void dispose() {
+    institusiController.dispose();
+    pembayaranController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,34 +38,134 @@ class PendidikanScreen extends StatelessWidget {
         ),
         elevation: 0,
       ),
-      body: Center(
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 100),
-            Image.asset(
-              'assets/404.png',
-              width: 200,
-              height: 200,
+      // body: Center(
+      //   child: Column(
+      //     // mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       const SizedBox(height: 100),
+      //       Image.asset(
+      //         'assets/404.png',
+      //         width: 200,
+      //         height: 200,
+      //       ),
+      //       Text(
+      //         'Oops',
+      //         style: blackFont16.copyWith(color: Colors.black, fontSize: 18),
+      //       ),
+      //       const SizedBox(height: 10),
+      //       Text(
+      //         'Mohon maaf, terjadi gangguan pada sistem',
+      //         style: blackFont16.copyWith(color: Colors.grey, fontSize: 14),
+      //       ),
+      //       Text(
+      //         'kami. Mohon hubungi layanan pelanggan',
+      //         style: blackFont16.copyWith(color: Colors.grey, fontSize: 14),
+      //       ),
+      //       Text(
+      //         'untuk pengaduan masalah Anda.',
+      //         style: blackFont16.copyWith(color: Colors.grey, fontSize: 14),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              Text(
+                'Institusi',
+                style: blackFont12.copyWith(fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: TextField(
+                  controller: institusiController,
+                  decoration: InputDecoration(
+                      prefixIcon: InkWell(
+                        onTap: () {
+                          // Navigator.push(
+                          //   context,
+                          //   MaterialPageRoute(
+                          //     builder: (context) => const RegionScreen(),
+                          //   ),
+                          // );
+                        },
+                        child: const Icon(Icons.search),
+                      ),
+                      hintText: 'Cari Wilayah',
+                      hintStyle: blackFont12.copyWith(color: Colors.grey),
+                      focusedBorder: InputBorder.none),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'No. Pelanggan',
+                style: blackFont12.copyWith(fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(height: 5),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: TextField(
+                  controller: pembayaranController,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintStyle: blackFont12.copyWith(color: Colors.grey),
+                    hintText: 'Masukkan No Pembayaran',
+                  ),
+                ),
+              ),
+              Center(
+                child: SizedBox(
+                  width: 326,
+                  height: 290,
+                  child: Image.asset('assets/icon_pdam1.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 30, left: 24, right: 24),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: 52,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: blueColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
             ),
-            Text(
-              'Oops',
-              style: blackFont16.copyWith(color: Colors.black, fontSize: 18),
+            onPressed: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(
+              //     builder: (context) => const PaymentDetailPdam(),
+              //   ),
+              // );
+            },
+            child: Text(
+              'Lanjutkan',
+              style: whiteFont14,
             ),
-            const SizedBox(height: 10),
-            Text(
-              'Mohon maaf, terjadi gangguan pada sistem',
-              style: blackFont16.copyWith(color: Colors.grey, fontSize: 14),
-            ),
-            Text(
-              'kami. Mohon hubungi layanan pelanggan',
-              style: blackFont16.copyWith(color: Colors.grey, fontSize: 14),
-            ),
-            Text(
-              'untuk pengaduan masalah Anda.',
-              style: blackFont16.copyWith(color: Colors.grey, fontSize: 14),
-            ),
-          ],
+          ),
         ),
       ),
     );
