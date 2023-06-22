e6c887c4f0e66d8c92e731109dbffc3b23c48a19
Ahmad Alif <ahmadaliffauzan@gmail.com>
2023-06-22 13:32:10 +0700

    edit provider for show name, phone, and balance in home screen

--- capstone_flutter/lib/view_model/user_provider/user_provider.dart
+++ capstone_flutter/lib/view_model/user_provider/user_provider.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProvider with ChangeNotifier {
   String name = '';
   String phone = '';
   int balance = 0;
 
-  void updateUserInfo(String newName, String newPhone, int newBalance) {
+  UserProvider() {
+    //ntuk mengambil nilai dari SharedPreferences saat inisialisasi
+    getSavedUserInfo();
+  }
+
+  Future<void> getSavedUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    name = prefs.getString('name') ?? '';
+    phone = prefs.getString('phone') ?? '';
+    balance = prefs.getInt('balance') ?? 0;
+    notifyListeners();
+  }
+
+  void updateUserInfo(String newName, String newPhone, int newBalance) async {
     name = newName;
     phone = newPhone;
     balance = newBalance;
+
+    // Simpan nilai ke SharedPreferences setelah diperbarui
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('name', name);
+    await prefs.setString('phone', phone);
+    await prefs.setInt('balance', balance);
+
     notifyListeners();
   }
 }
